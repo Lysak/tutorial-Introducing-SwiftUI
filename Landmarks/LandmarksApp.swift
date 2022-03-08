5ac088ad244a571a0ecc580b7ab109808b162e24
Dmytrii Lysak <dlysak@live.com>
2022-03-08 14:55:36 +0200

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lysak on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
